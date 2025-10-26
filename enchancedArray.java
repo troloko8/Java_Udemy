d9c029c17dfce2e949d8482cf3e4d56ed46b53a4
Nathan_Ipad <troloko8@gmail.com>
2025-10-26 22:07:31 +0200

    enchanced array

---
+++ enchancedArray.java
@@ -0,0 +1,15 @@
+
+public class enchancedArray {
+    public static void main(String a[]) 
+    {
+        int nums[] = new int[3];
+
+        nums[0] = 3;
+        nums[1] = 4;
+        nums[2] = 2;
+
+        for (int n : nums) {
+            System.err.println("The num: " + n);
+        }
+    }
+}
