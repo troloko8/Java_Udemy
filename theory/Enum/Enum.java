46fefe5969d99a2c269021ae7c364caed976ad8b
Nathan_Ipad <troloko8@gmail.com>
2025-11-11 22:34:09 +0200

    special uses enum with switch

--- theory/Enum/Enum.java
+++ theory/Enum/Enum.java
@@ -6,7 +6,7 @@ enum Status {
 
 public class Enum {
     public static void main(String a[]) {
-        Status s = Status.Running;
+        Status s = Status.Successs;
 
         System.err.println(s);
 
@@ -14,8 +14,28 @@ public class Enum {
 
         System.err.println(ss[0]);
 
-        for(Status c : ss) {
+        for  (Status c : ss) {
             System.err.println(c);
+        }
+        
+        System.err.println(" _ _- - _ _ - -  _ __. __ _ _ - _ - ");
+
+        switch (s) {
+            case Running:
+                System.out.println("All good");
+                break;
+        
+            case Failed:
+                System.out.println("Try again");
+                break;
+        
+            case Pending:
+                System.out.println("Please wait");
+                break;
+        
+            default:
+                System.out.println("Done");
+                break;
         }
     }
 }
