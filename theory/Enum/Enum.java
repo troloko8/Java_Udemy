a8eb3182bd6d8f6aa2abfd3c0dcd5a3fcd69ac97
Nathan_Ipad <troloko8@gmail.com>
2025-11-11 22:52:34 +0200

    advanced enum

--- theory/Enum/Enum.java
+++ theory/Enum/Enum.java
@@ -4,41 +4,77 @@ enum Status {
     Running, Failed, Pending, Successs;
 }
 
+enum Laptop {
+    Macbook(2000), XPS, Surface(1800), ThinkPad(2200);
+
+    private int price;
+
+    Laptop() {
+        price = 199;
+    }
+
+    Laptop(int price) {
+        this.price = price;
+
+        System.err.println("in Laprop : " + this.name());
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
+
 public class Enum {
     public static void main(String a[]) {
-        Status s = Status.Successs;
 
-        int numOfS = s.ordinal();
+        // Laptop lap = Laptop.Macbook;
 
-        System.err.println(s);
-        System.err.println("numOfS : " + numOfS);
+        // System.err.println(lap.getPrice());
 
-        Status[] ss = Status.values();
-
-        System.err.println(ss[0]);
-
-        for  (Status c : ss) {
-            System.err.println(c);
+        for(Laptop aLap : Laptop.values()) {
+            System.err.println(aLap  + " : " + aLap.getPrice());
         }
-        
-        System.err.println(" _ _- - _ _ - -  _ __. __ _ _ - _ - ");
 
-        switch (s) {
-            case Running:
-                System.out.println("All good");
-                break;
+        // OLDone basic
+        // Status s = Status.Successs;
+
+        // int numOfS = s.ordinal();
+
+        // System.err.println(s);
+        // System.err.println("numOfS : " + numOfS);
+
+        // Status[] ss = Status.values();
+
+        // System.err.println(ss[0]);
+
+        // for  (Status c : ss) {
+        //     System.err.println(c);
+        // }
         
-            case Failed:
-                System.out.println("Try again");
-                break;
+        // System.err.println(" _ _- - _ _ - -  _ __. __ _ _ - _ - ");
+
+        // switch (s) {
+        //     case Running:
+        //         System.out.println("All good");
+        //         break;
         
-            case Pending:
-                System.out.println("Please wait");
-                break;
+        //     case Failed:
+        //         System.out.println("Try again");
+        //         break;
         
-            default:
-                System.out.println("Done");
-                break;
-        }
+        //     case Pending:
+        //         System.out.println("Please wait");
+        //         break;
+        
+        //     default:
+        //         System.out.println("Done");
+        //         break;
+        // }
+
+        // System.err.println("Is Enum a class: " + s.getClass().getSuperclass());
     }
 }
