bc27b1e0f1b371bac1bd5bd3a7e23911b8516a90
Nathan_N <nathan.tytarenko@consumrz.com>
2025-11-12 13:18:39 +0200

    lambda expression

--- theory/Interface/FunctionalInterface.java
+++ theory/Interface/FunctionalInterface.java
@@ -2,23 +2,28 @@ package Interface;
 
 // @FunctionalInterface
 interface A {
-    void show();
+    void show(int i);
 }
 
-class B implements A {
-    public void show() {
-        System.err.println("In show");
-    }
-}
+// class B implements A {
+//     public void show() {
+//         System.err.println("In show");
+//     }
+// }
+
 public class FunctionalInterface {
     public static void main(String a[]) {
         // A obj = new B()
-        A obj = new A(){
-            public void show() {
-                System.err.println("In show");
-            }
-        };
+        // A obj = new A(){
+        //     public void show() {
+        //         System.err.println("In show");
+        //     }
+        // };
 
-        obj.show();
+        // Lambda expression
+
+        A obj = i -> System.err.println("In show " + i);
+
+        obj.show(109);
     }
 }
