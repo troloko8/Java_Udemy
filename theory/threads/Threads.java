23170eff1ab1005a7e7b8c899f519b127192bfae
Nathan_Ipad <troloko8@gmail.com>
2025-11-15 22:57:13 +0200

    threads priority and sleep mathod

--- theory/threads/Threads.java
+++ theory/threads/Threads.java
@@ -6,6 +6,13 @@ class A extends Thread {
     public void run() {
         for (int i = 1; i <= 100; i++) {
             System.err.println("Hi " + i);
+            
+            try {
+                // Thred.sleep method is used to pause the execution of current thread for specified milliseconds
+                Thread.sleep(1);
+            } catch (Exception e) {
+                System.err.println(e);
+            }
         }
     }
 }
@@ -14,6 +21,12 @@ class B extends Thread {
     public void run() {
         for (int i = 1; i <= 100; i++) {
             System.err.println("Hello " + i);
+
+            try {
+                Thread.sleep(1);
+            } catch (Exception e) {
+                System.err.println(e);
+            }
         }
     }
 }
@@ -22,6 +35,12 @@ public class Threads {
     public static void main(String[] args) {
         A a = new A();
         B b = new B();
+
+        // {NOTE}
+        // default priority is 5
+        // from 1 to 10
+        // System.err.println("Priority of thread a: " + a.getPriority());
+
         // to start the thread we need to call start method
         a.start();
         b.start();
