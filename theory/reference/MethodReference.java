c451a09f551e0500ae432b0b639f899135f815bd
Nathan_Ipad <troloko8@gmail.com>
2025-11-22 19:39:34 +0200

    constructor reference

--- theory/reference/MethodReference.java
+++ theory/reference/MethodReference.java
@@ -1,7 +1,38 @@
 package reference;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+class Student {
+    int age;
+    String name;
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
 
 public class MethodReference {
     public static void main(String[] args) {
@@ -12,6 +43,22 @@ public class MethodReference {
             .map(String::toUpperCase)
             .toList();
             
-        uNames.forEach(System.out::println);
+        // uNames.forEach(System.out::println);
+
+        // constructor reference
+        List<Student> students = new ArrayList<>();
+
+        for (String name : names) {
+            students.add(new Student(name));
+        }
+
+        students = names.stream()
+            .map(name -> new Student(name))
+                .toList();
+            
+        // constructor reference
+        students = names.stream()
+            .map(Student::new)
+                .toList();
     }
 }
