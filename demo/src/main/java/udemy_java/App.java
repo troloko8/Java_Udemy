7232c67c05eb119f979c328f77d90e07c1b437e8
Nathan_Ipad <troloko8@gmail.com>
2025-12-10 21:35:56 +0200

    the full script of fetching data from postgres

--- demo/src/main/java/udemy_java/App.java
+++ demo/src/main/java/udemy_java/App.java
@@ -1,7 +1,9 @@
 package udemy_java;
 
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -19,9 +21,26 @@ public class App {
         String username = "postgres";
         String pass = "1001";
 
-        try (Connection con = DriverManager.getConnection(url, username, pass)) {
+        Connection con = null;
+
+        try {
+            con = DriverManager.getConnection(url, username, pass);
             System.err.println("Connected to the PostgreSQL server successfully.");
-            // Здесь вы можете выполнять операции с базой данных, используя объект `con`
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        String sql = "SELECT \"sName\" FROM student WHERE \"sID\" = 1"; // Исправленный SQL-запрос
+        Statement st = null;
+        ResultSet res = null;
+        
+        try {
+            st = con.createStatement();
+            res = st.executeQuery(sql);
+            res.next();
+            String name = res.getString("sName");
+            System.err.println("Name of the student is " + name);
+            con.close();    
         } catch (SQLException e) {
             e.printStackTrace();
         }
