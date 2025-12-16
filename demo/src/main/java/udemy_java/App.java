e94f3b5e8c37fa9c9d87cfe0024b3f0b4f963d84
Nathan_N <nathan.tytarenko@consumrz.com>
2025-12-16 12:28:30 +0200

    CRUD with hibernate

--- demo/src/main/java/udemy_java/App.java
+++ demo/src/main/java/udemy_java/App.java
@@ -20,14 +20,32 @@ public class App {
         try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
 
+            // CREATE
             // session.persist(s1);
             // session.getTransaction().commit();
-
             // System.err.println(s1);
 
-            s2 = session.find(StudentHibrnate.class, 2);
-            
-            System.err.println(s2);
+            // GET
+            // s2 = session.find(StudentHibrnate.class, 2);
+            // System.err.println(s2);
+
+            // UPDATE
+            // s1.setName("LALA3");
+            // s1.setRollNo(3);
+            // s1.setAge(2);
+
+            // // selecting 
+            // session.merge(s1);
+            // // updating
+            // session.getTransaction().commit();
+            // System.err.println(s1);
+
+            // DELETE
+
+            s1 = session.find(StudentHibrnate.class, 3);
+
+            session.remove(s1);
+            session.getTransaction().commit();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
