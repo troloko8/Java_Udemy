c7e7c2eb8bf321d61eca70c95d22a93293a664a2
Nathan_N <nathan.tytarenko@consumrz.com>
2026-01-24 12:55:47 +0200

    Common Spring first step

---
+++ demo/demo/src/main/java/com/example/Alien.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Alien {
+
+    public void code() {
+        System.err.println("Code..");
+    }
+    
+}
---
+++ demo/demo/src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        ApplicationContext context = new ClassPathXmlApplicationContext();
+        Alien alien = (Alien) context.getBean("alien");
+
+    }
+}
