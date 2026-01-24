fa570b51526056bb6045dee2c412fb6a4379dcca
Nathan_N <nathan.tytarenko@consumrz.com>
2026-01-24 12:08:30 +0200

    autowiring concept

--- demo/src/main/java/com/example/demo/Alien.java
+++ demo/src/main/java/com/example/demo/Alien.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Component
 public class Alien {
+
+    @Autowired
+    Laptop laptop;
+
     public void code() {
+        laptop.compiling();
         System.err.println("code...");
     }
 }
---
+++ demo/src/main/java/com/example/demo/Laptop.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    public void compiling() {
+        System.err.println("compiling");
+    }
+}
