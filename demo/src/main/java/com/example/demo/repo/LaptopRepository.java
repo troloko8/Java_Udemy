eab543b041a5a98976ba3e9456a6f25aeedbf117
Nathan_N <nathan.tytarenko@consumrz.com>
2026-02-25 20:12:34 +0200

    nth

--- demo/src/main/java/com/example/demo/repo/LaptopRepository.java
+++ demo/src/main/java/com/example/demo/repo/LaptopRepository.java
@@ -1,4 +1,4 @@
-package com.example.demo.service;
+package com.example.demo.repo;
 
 import org.springframework.stereotype.Repository;
 
--- demo/src/main/java/com/example/demo/service/LaptopService.java
+++ demo/src/main/java/com/example/demo/service/LaptopService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Laptop;
+import com.example.demo.repo.LaptopRepository;
 
 @Service
 public class LaptopService {
