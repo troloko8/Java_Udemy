2560b6316273848dd623d058dcd2a99cbc3c60af
Nathan_N <nathan.tytarenko@consumrz.com>
2026-02-25 20:11:55 +0200

    Reposityry Layer

--- demo/src/main/java/com/example/demo/service/LaptopService.java
+++ demo/src/main/java/com/example/demo/service/LaptopService.java
@@ -1,13 +1,19 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Laptop;
 
 @Service
 public class LaptopService {
+
+    @Autowired
+    private LaptopRepository repo;
+
     public void addLaptop(Laptop lap) {
         System.err.println("adding Laptop");
+        this.repo.save();
     }
 
     public boolean isGoodForProg(Laptop lap) {
