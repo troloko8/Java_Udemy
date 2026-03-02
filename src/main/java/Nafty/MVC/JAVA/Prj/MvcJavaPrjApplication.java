97ab33d12560557459f8ff7b33ee8b1d0935f596
Nathan_N <nathan.tytarenko@consumrz.com>
2026-03-02 20:57:30 +0200

    init first MVC proj and created jsp file

---
+++ src/main/java/Nafty/MVC/JAVA/Prj/MvcJavaPrjApplication.java
@@ -0,0 +1,14 @@
+package Nafty.MVC.JAVA.Prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcJavaPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcJavaPrjApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/Nafty/MVC/JAVA/Prj/MvcJavaPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package Nafty.MVC.JAVA.Prj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcJavaPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
