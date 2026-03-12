5b70461ca294b38e93777d103e4dad2817d6ce2b
Nathan_N <nathan.tytarenko@consumrz.com>
2026-03-12 18:58:48 +0200

    views for proj

--- src/main/java/com/telusko/JobApp/JobAppApplication.java
+++ src/main/java/com/telusko/JobApp/JobAppApplication.java
@@ -1,13 +1,15 @@
-package cnaftyDemo.jopProject;
+package com.telusko.JobApp;
 
 import org.springframework.boot.SpringApplication;
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JopProjectApplication {
+public class JobAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JopProjectApplication.class, args);
+		SpringApplication.run(JobAppApplication.class, args);
 	}
 
 }
--- src/test/java/com/telusko/SpringBootWeb1/SpringBootWeb1ApplicationTests.java
+++ src/test/java/com/telusko/SpringBootWeb1/SpringBootWeb1ApplicationTests.java
@@ -1,10 +1,10 @@
-package cnaftyDemo.jopProject;
+package com.telusko.SpringBootWeb1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JopProjectApplicationTests {
+class SpringBootWeb1ApplicationTests {
 
 	@Test
 	void contextLoads() {
