47ff2af68777716ec424de7c8cc0556740120ee0
Nathan_N <nathan.tytarenko@consumrz.com>
2026-03-12 19:10:55 +0200

    addjob view

--- src/main/java/com/telusko/JobApp/controller/JobController.java
+++ src/main/java/com/telusko/JobApp/controller/JobController.java
@@ -1,47 +1,33 @@
 package com.telusko.JobApp.controller;
 
-import com.telusko.JobApp.model.JobPost;
-import com.telusko.JobApp.service.JobService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 
 @Controller
 public class JobController {
 
-    @Autowired
-    private JobService service;
-
-
-    @GetMapping({"/", "home"})
+    @RequestMapping({"/", "/home"})
     public String home() {
         return "home";
     }
 
-
-    @GetMapping("addjob")
+    @RequestMapping("addjob")
     public String addJob() {
         return "addjob";
     }
 
+    // @RequestMapping({"/", "/home"})
+    // public String home() {
+    //     return "home";
+    // }
 
-    @PostMapping("handleForm")
-    public String handleForm(JobPost jobPost) {
-        service.addJob(jobPost);
-        return "success";
+    // @RequestMapping({"/", "/home"})
+    // public String home() {
+    //     return "home";
+    // }
 
-    }
-
-    @GetMapping("viewalljobs")
-    public String viewJobs(Model m) {
-        List<JobPost> jobs = service.getAllJobs();
-        m.addAttribute("jobPosts", jobs);
-
-        return "viewalljobs";
-    }
 
 }
+
