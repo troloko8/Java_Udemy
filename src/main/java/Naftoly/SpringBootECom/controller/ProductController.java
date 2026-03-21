9dab313ccb159c0f76315ef2912b0b7ca339681c
Nathan_N <nathan.tytarenko@consumrz.com>
2026-03-21 13:46:40 +0200

    @ResponseEntity anotation (http status)

--- src/main/java/Naftoly/SpringBootECom/controller/ProductController.java
+++ src/main/java/Naftoly/SpringBootECom/controller/ProductController.java
@@ -8,6 +8,8 @@ import Naftoly.SpringBootECom.service.ProductService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +24,8 @@ public class ProductController {
     private ProductService productService;
     
     @GetMapping("/products")
-    public List<Product> getProducts() {
-        return productService.getAllProducts();
+    public ResponseEntity<List<Product>> getProducts() {
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
     }
     
 }
