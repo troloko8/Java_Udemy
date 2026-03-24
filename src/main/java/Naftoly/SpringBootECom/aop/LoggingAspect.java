e440b2110a44193443376645b2e350533c9258bc
Nathan_N <nathan.tytarenko@consumrz.com>
2026-03-24 21:27:14 +0200

    AOP After advice

--- src/main/java/Naftoly/SpringBootECom/aop/LoggingAspect.java
+++ src/main/java/Naftoly/SpringBootECom/aop/LoggingAspect.java
@@ -3,6 +3,9 @@ package Naftoly.SpringBootECom.aop;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -25,6 +28,21 @@ public class LoggingAspect {
     @Before("execution(* Naftoly.SpringBootECom.service.ProductService.getProductById(..)) || execution(* Naftoly.SpringBootECom.service.ProductService.getAllProducts(..))")
     public void logMethodCall(JoinPoint joinPoint) {
         LOGGER.info("Entering method: " + joinPoint);
+    }
+
+    @After("execution(* Naftoly.SpringBootECom.service.ProductService.getProductById(..)) || execution(* Naftoly.SpringBootECom.service.ProductService.getAllProducts(..))")
+    public void logMethodExecuted(JoinPoint joinPoint) {
+        LOGGER.info("Method executed: " + joinPoint);
+    }
+
+    @AfterThrowing("execution(* Naftoly.SpringBootECom.service.ProductService.getProductById(..)) || execution(* Naftoly.SpringBootECom.service.ProductService.getAllProducts(..))")
+    public void logMethodCrash(JoinPoint joinPoint) {
+        LOGGER.info("Method crashed: " + joinPoint);
+    }
+
+    @AfterReturning("execution(* Naftoly.SpringBootECom.service.ProductService.getProductById(..)) || execution(* Naftoly.SpringBootECom.service.ProductService.getAllProducts(..))")
+    public void logMethodReturn(JoinPoint joinPoint) {
+        LOGGER.info("Method returned: " + joinPoint);
     }
     
 }
