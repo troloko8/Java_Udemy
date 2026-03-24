617694d655d4ccf4e69ebe14be0e18e1b121f392
Nathan_N <nathan.tytarenko@consumrz.com>
2026-03-24 21:13:30 +0200

    @Before practice of AOP

--- src/main/java/Naftoly/SpringBootECom/aop/LoggingAspect.java
+++ src/main/java/Naftoly/SpringBootECom/aop/LoggingAspect.java
@@ -2,12 +2,24 @@ package Naftoly.SpringBootECom.aop;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
 public class LoggingAspect {
 
     private static final Logger LOGGER = Logger.getLogger(LoggingAspect.class.getName());
 
-    public void logMethodCall(String methodName) {
-        LOGGER.info("Entering method: " + methodName);
+    // return type, class name, method name, and (parameters)
+    // all all.all ..all
+
+    // @Before("execution(* *.*(..))")
+    @Before("execution(* Naftoly.SpringBootECom.service.ProductService.*(..))")
+    public void logMethodCall(JoinPoint joinPoint) {
+        LOGGER.info("Entering method: " + joinPoint);
     }
     
 }
