99661e46ce94bfcd9bee0054d0cbb1592e3bf2f7
Nathan_N <nathan.tytarenko@consumrz.com>
2026-03-25 19:24:05 +0200

    sucurity example

---
+++ src/main/java/Nathan/spring_security/HelloController.java
@@ -0,0 +1,16 @@
+package Nathan.spring_security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("hello")
+	public String greet( HttpServletRequest request) {
+
+		return "Hello World " + request.getSession().getId();
+	}
+}
