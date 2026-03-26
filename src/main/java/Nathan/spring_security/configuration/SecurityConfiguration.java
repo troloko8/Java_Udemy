f3f51cfd4ff58f0fe2d959f0de67ac7cdfdb8c17
Nathan_N <nathan.tytarenko@consumrz.com>
2026-03-26 18:37:33 +0200

    auth 2 users

--- src/main/java/Nathan/spring_security/configuration/SecurityConfiguration.java
+++ src/main/java/Nathan/spring_security/configuration/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package Nathan.spring_security.configuration;
 
+import org.springframework.boot.security.autoconfigure.SecurityProperties.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,6 +8,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -33,5 +37,24 @@ public class SecurityConfiguration   {
 
         return http.build();
     }
-    
+
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user = User
+            .withDefaultPasswordEncoder()
+            .username("nathan")
+            .password("1234")
+            .roles("USER")
+            .build();
+
+
+        UserDetails admin = User
+            .withDefaultPasswordEncoder()
+            .username("alice")
+            .password("1234")
+            .roles("ADMIN")
+            .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 }
