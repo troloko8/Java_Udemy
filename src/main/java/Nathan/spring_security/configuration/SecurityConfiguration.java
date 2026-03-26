f9ec9e50ce552dbf710f36a868a958c24a973c27
Nathan_N <nathan.tytarenko@consumrz.com>
2026-03-26 18:13:57 +0200

    nth

--- src/main/java/Nathan/spring_security/configuration/SecurityConfiguration.java
+++ src/main/java/Nathan/spring_security/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,6 +15,15 @@ public class SecurityConfiguration   {
     
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // Customizer<CsrfConfigurer<HttpSecurity>> csrfCustomizer = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+        //     @Override
+        //     public void customize(CsrfConfigurer<HttpSecurity> httpSecurityCsrfConfigurer) {
+        //         httpSecurityCsrfConfigurer.disable();
+        //     }
+        // };
+
+        // lambda solution
         http.csrf(customizer -> customizer.disable());
         http.authorizeHttpRequests(requsts -> requsts.anyRequest().authenticated());
         http.formLogin(Customizer.withDefaults());
