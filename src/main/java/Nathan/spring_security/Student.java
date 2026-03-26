5179c977016e333c2f70a5fae241d1d2db7056fa
Nathan_N <nathan.tytarenko@consumrz.com>
2026-03-26 09:29:44 +0200

    sending CSRF token

---
+++ src/main/java/Nathan/spring_security/Student.java
@@ -0,0 +1,15 @@
+package Nathan.spring_security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+    private String tech;
+    
+}
