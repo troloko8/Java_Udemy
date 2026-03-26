b3d39331c7e75e07b790437510ec5313168f4212
Nathan_N <nathan.tytarenko@consumrz.com>
2026-03-26 10:00:31 +0200

    doing a config through config object using lambda expression

---
+++ src/main/java/Nathan/spring_security/configuration/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package Nathan.spring_security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration   {
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(customizer -> customizer.disable());
+        http.authorizeHttpRequests(requsts -> requsts.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+    
+}
