6e1762b43ba8fbf8c90926942d7605e254362493
Nathan_N <nathan.tytarenko@consumrz.com>
2026-03-30 10:33:08 +0300

    set possword encoder

--- src/main/java/Nathan/spring_security/configuration/SecurityConfiguration.java
+++ src/main/java/Nathan/spring_security/configuration/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -40,7 +41,9 @@ public class SecurityConfiguration   {
     public AuthenticationProvider authProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
 
-        provider.setPasswordEncoder(passwordEncoder);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        // provider.setPasswordEncoder(passwordEncoder);
+        
 
         return provider;
     }
