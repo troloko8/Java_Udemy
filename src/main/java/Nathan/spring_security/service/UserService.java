7f2889de74225515d526cc9a561db7b8580e3245
Nathan_N <nathan.tytarenko@consumrz.com>
2026-03-30 10:25:04 +0300

    bCrypt encoding for user registration

--- src/main/java/Nathan/spring_security/service/UserService.java
+++ src/main/java/Nathan/spring_security/service/UserService.java
@@ -1,6 +1,7 @@
 package Nathan.spring_security.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import Nathan.spring_security.dao.UserRepo;
@@ -11,8 +12,12 @@ public class UserService {
 
     @Autowired
     private UserRepo repo;
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     public User save(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        System.err.println(user.getPassword());
+        
         repo.save(user);
         return user;
     }
